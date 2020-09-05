c03cc3be3b5d0392d757ed0d741d7bd98091672c
codekoch <45600398+codekoch@users.noreply.github.com>
2020-09-05 15:07:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,11 +28,22 @@ user0
 Y
 EOF
 
+#### set group rights
+usermod -a -G adm,dialout,fax,cdrom,floppy,tape,dip,video,plugdev user0 
+
 #### set selfhealing home of user user0
 sudo cp scripts/resethomedir.sh /etc/init.d/
 chmod 777 /etc/init.d/resethomedir.sh
 sudo update-rc.d resethomedir.sh defaults
 sudo /etc/init.d/resethomedir.sh save
+
+#### set autologin of user user0
+sudo mkdir /etc/lightdm/lightdm.conf.d//
+sudo echo '[Seat:*]' > /etc/lightdm/lightdm.conf.d/60-autologin.conf
+sudo echo 'autologin-user=user0' >> /etc/lightdm/lightdm.conf.d/60-autologin.conf
+sudo echo 'autologin-user-timeout=0' >> /etc/lightdm/lightdm.conf.d/60-autologin.conf
+sudo echo "# don't sleep the screen" >> /etc/lightdm/lightdm.conf.d/60-autologin.conf
+sudo echo "xserver-command=X -s 0 dpms">> /etc/lightdm/lightdm.conf.d/60-autologin.conf
 
 yellow_msg "->DONE!"
 
