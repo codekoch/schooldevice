ca05584c912135facae156e43998b40e5e8eb1e5
codekoch <45600398+codekoch@users.noreply.github.com>
2020-09-10 16:09:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,12 +32,14 @@ EOF
 usermod -a -G adm,dialout,fax,cdrom,floppy,tape,dip,video,plugdev user0 
 
 #### set selfhealing home of user user0
+yellow_msg "setup selfhealing account for user0..."
 sudo cp scripts/resethomedir.sh /etc/init.d/
 chmod 777 /etc/init.d/resethomedir.sh
 sudo update-rc.d resethomedir.sh defaults
 sudo /etc/init.d/resethomedir.sh save
 
 #### set autologin of user user0
+yellow_msg "set autologin for user0"
 sudo mkdir /etc/lightdm/lightdm.conf.d/
 sudo echo '[Seat:*]' > /etc/lightdm/lightdm.conf.d/60-autologin.conf
 sudo echo 'autologin-user=user0' >> /etc/lightdm/lightdm.conf.d/60-autologin.conf
@@ -46,6 +48,7 @@ sudo echo "# don't sleep the screen" >> /etc/lightdm/lightdm.conf.d/60-autologin
 sudo echo "xserver-command=X -s 0 dpms">> /etc/lightdm/lightdm.conf.d/60-autologin.conf
 
 #### Notify that you are in a selfhealing account
+yellow_msg "adding notfify-script"
 sudo mkdir /home/.saves/user0/.config/
 sudo mkdir /home/.saves/user0/.config/autostart/
 sudo echo '[Desktop Entry]' > /home/.saves/user0/.config/autostart/notify.desktop
@@ -59,6 +62,7 @@ sudo cp scripts/notify.sh /home/.saves/user0/
 sudo chmod 755  /home/.saves/user0/notify.sh
 
 #### set new user background
+yellow_msg "set background for user0"
 sudo cp schooldevice.png /usr/share/backgrounds/xfce/
 sudo chmod 755 /usr/share/backgrounds/xfce/schooldevice.png
 sudo echo '#!/bin/bash' > /home/.saves/user0/setbackground.sh
