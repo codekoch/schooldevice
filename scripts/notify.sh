227294705694a5edaa1fb09998bcd2973902ae3f
codekoch <45600398+codekoch@users.noreply.github.com>
2020-09-13 16:13:02 +0200

    Update notify.sh

--- scripts/notify.sh
+++ scripts/notify.sh
@@ -2,7 +2,7 @@
  
 sleep 10                                               
  
-notify-send "ATTENTION:" "All local data will be lost during logout or restart! Make sure your data is backed up in the cloud or on an external device if necessary."   
+notify-send "ATTENTION:" "All local data will be lost during logout or restart!\nMake sure your data is backed up in the cloud or on an external device if necessary."   
  
 sleep 60                                                
  
