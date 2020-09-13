9bfae9119807a1967c2979ca23c5400b1ef8ff87
codekoch <45600398+codekoch@users.noreply.github.com>
2020-09-13 12:34:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,27 +16,27 @@ echo -e "\\033[34;1m${@}\033[0m"
 }
 
 #### installation of timeshift
-#if [ $1 = "update" ]; then
-#  yellow_msg "Updating..."
-#  git fetch --all
-#  git reset --hard origin/master
-#  $snapshotname = `cat snapshotname.txt` 
-#  yellow_msg "Restore system with snapshot $snapshotname..."
-#  sudo timeshift --restore --snap $snapshotname --yes
-#  yellow_msg "Continue with new installation of schooldevice ..."
-#else
-#   if [ -f ./snapshotname.txt ]; then
-#        sudo timeshift --delete-all --yes
-#        $snapshotname = `cat snapshotname.txt`
-#        sudo timeshift --delete --snapshot $snapshotname
-#   else
-#        yellow_msg "adding timeshift ability ..." 
-#        sudo apt-get install -y timeshift
-#    fi  
-#   yellow_msg "Creating snapshot of current system ...[this will take a while...time for a coffee!]"  
-#   sudo timeshift --create --yes
-#   sudo timeshift --list | grep -i ">" | awk '{print $3}' > ./snapshotname.txt
-#fi
+if [ $1 = "update" ]; then
+  yellow_msg "Updating..."
+  git fetch --all
+  git reset --hard origin/master
+  $snapshotname = `cat snapshotname.txt` 
+  yellow_msg "Restore system with snapshot $snapshotname..."
+  sudo timeshift --restore --snapshot $snapshotname --yes
+  yellow_msg "Continue with new installation of schooldevice ..."
+else
+   if [ -f ./snapshotname.txt ]; then
+        sudo timeshift --delete-all --yes
+        $snapshotname = `cat snapshotname.txt`
+        sudo timeshift --delete --snapshot $snapshotname
+   else
+        yellow_msg "adding timeshift ability ..." 
+        sudo apt-get install -y timeshift
+    fi  
+   yellow_msg "Creating snapshot of current system ...[this will take a while...time for a coffee!]"  
+   sudo timeshift --create --yes
+   sudo timeshift --list | grep -i ">" | awk '{print $3}' > ./snapshotname.txt
+fi
 
 #### add user user
 yellow_msg "adding user user0 with password user0..." 
