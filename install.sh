8e897ab3a7e6a528b2debc158953b8a2539f1463
codekoch <45600398+codekoch@users.noreply.github.com>
2020-09-14 18:36:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -74,19 +74,17 @@ usermod -a -G adm,dialout,fax,cdrom,floppy,tape,dip,video,plugdev user0
 #### set selfhealing home of user user0
 yellow_msg "setup selfhealing account for user0..."
 sudo cp scripts/resethomedir.sh /etc/init.d/
-chmod 777 /etc/init.d/resethomedir.sh
-sudo update-rc.d resethomedir.sh defaults
+chmod 755 /etc/init.d/resethomedir.sh
 sudo /etc/init.d/resethomedir.sh save
-#sudo  sed -i 's/Required-Stop:     $local_fs $remote_fs dbus/Required-Stop:     $local_fs $remote_fs dbus resethomedir.sh/g' /etc/init.d/lightdm
 
 #### set autologin of user user0
 yellow_msg "set autologin for user0"
 sudo mkdir /etc/lightdm/lightdm.conf.d/
 sudo echo '[Seat:*]' > /etc/lightdm/lightdm.conf.d/60-autologin.conf
+sudo echo 'display-setup-script=/etc/init.d/resethomedirstart.sh' >> /etc/lightdm/lightdm.conf.d/60-autologin.conf
 sudo echo 'autologin-user=user0' >> /etc/lightdm/lightdm.conf.d/60-autologin.conf
 sudo echo 'autologin-user-timeout=0' >> /etc/lightdm/lightdm.conf.d/60-autologin.conf
 sudo echo "# don't sleep the screen" >> /etc/lightdm/lightdm.conf.d/60-autologin.conf
-sudo echo 'display-setup-script=/etc/init.d/resethomedir.sh' >> /etc/lightdm/lightdm.conf.d/60-autologin.conf
 sudo echo "xserver-command=X -s 0 dpms">> /etc/lightdm/lightdm.conf.d/60-autologin.conf
 
 #### Notify that you are in a selfhealing account
