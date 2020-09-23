ad33e9c3fda2e156b0aa6dc887b261061ddf51dd
codekoch <45600398+codekoch@users.noreply.github.com>
2020-09-23 17:30:22 +0200

    Update loadSession.sh

--- scripts/loadSession.sh
+++ scripts/loadSession.sh
@@ -2,7 +2,7 @@
 #!/bin/bash  
 path=`zenity --file-selection --directory --title="Choose folder with session backup" --filename=/media/user0/` 
 cd /
-rsync -a $path/ /home/user0/ --delete
+rsync -a $path/ /home/user0/ 
 notify-send "Session loaded" "from $path"
  
 sleep 60                                                
