5529567b518a593240a31e72173555af72e4067f
codekoch <45600398+codekoch@users.noreply.github.com>
2020-09-24 15:22:51 +0200

    Update loadSession.sh

--- scripts/loadSession.sh
+++ scripts/loadSession.sh
@@ -3,6 +3,6 @@
 su user0 -c 'zenity --info --text "Please close all running aplications..." --title "ATTENTION!"' 
 path=`su user0 -c 'zenity --file-selection --directory --title="Choose folder with session backup" --filename=/media/user0/'` 
 cd /
-rsync -a --exclude=.cache $path/ /home/user0/ --delete | su user0 -c 'notify-send "Loading session" "please wait ..."'
+rsync -a --exclude=.cache --progress $path/ /home/user0/ --delete | su user0 -c 'notify-send "Loading session" "please wait ..."'
 su user0 -c 'zenity --info --text "Session loaded from '$path'" --title "Success!"' 
 
