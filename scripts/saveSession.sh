577af727bc91621f266a69d47919d76a31428df8
codekoch <45600398+codekoch@users.noreply.github.com>
2020-09-24 15:23:10 +0200

    Update saveSession.sh

--- scripts/saveSession.sh
+++ scripts/saveSession.sh
@@ -3,5 +3,5 @@
 su user0 -c 'zenity --info --text "Please close all running aplications...\n" --title "ATTENTION!"' 
 path=`su user0 -c 'zenity --file-selection --directory --title="Choose a folder for session backup" --filename=/media/user0/'` 
 cd /
-rsync -a --exclude=.cache /home/user0/ $path/ --delete | su user0 -c 'notify-send "Saving session" "please wait ..."'
+rsync -a --exclude=.cache --progress /home/user0/ $path/ --delete | su user0 -c 'notify-send "Saving session" "please wait ..."'
 su user0 -c 'zenity --info --text "Session saved to '$path'" --title "Success!"' 
