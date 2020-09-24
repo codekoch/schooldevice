2dde21dce7d6f38e77bdfdac3dec414599bfcbf8
codekoch <45600398+codekoch@users.noreply.github.com>
2020-09-24 17:28:29 +0200

    Update saveSession.sh

--- scripts/saveSession.sh
+++ scripts/saveSession.sh
@@ -4,5 +4,5 @@ path=`su user0 -c 'zenity --file-selection --directory --title="Choose a folder 
 cd /
 wget https://raw.githubusercontent.com/codekoch/schooldevice/master/scripts/rsync-homedir-local.txt -O rsync-homedir-local.txt
 chmod 777 rsync-homedir-local.txt
-rsync -a  --exclude-from=rsync-homedir-local.txt --progress /home/user0/ $path/ --delete | su user0 -c 'notify-send "Saving session" "please wait ..."'
+rsync -a  --exclude-from=rsync-homedir-local.txt --progress /home/user0/ $path/ --delete | su user0 -c 'su user0 -c 'zenity --title "Session" --text "Saving...(Please wait for Ok Button)" --progress --auto-kill'
 su user0 -c 'zenity --info --text "Session saved to '$path'" --title "Success!"' 
