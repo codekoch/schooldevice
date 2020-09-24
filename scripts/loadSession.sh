f58510df23f7298949c14c44a362a14535a47bbd
codekoch <45600398+codekoch@users.noreply.github.com>
2020-09-24 17:33:20 +0200

    Update loadSession.sh

--- scripts/loadSession.sh
+++ scripts/loadSession.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-su user0 -c 'zenity --info --text "Please close all running aplications..." --title "ATTENTION!"' 
+su user0 -c 'zenity --info --width 300 --text "Please close all running aplications...\nAnd insert an external device with FAT32 filesystem" --title "ATTENTION!"' 
 path=`su user0 -c 'zenity --file-selection --directory --title="Choose folder with session backup" --filename=/media/user0/'` 
 cd /
 wget https://raw.githubusercontent.com/codekoch/schooldevice/master/scripts/rsync-homedir-local.txt -O rsync-homedir-local.txt
 chmod 777 rsync-homedir-local.txt
-rsync -a  --exclude-from=rsync-homedir-local.txt --progress $path/ /home/user0/ --delete | su user0 -c 'zenity --title "Session" --text "Loading...(please wait for Ok Button)" --progress --auto-kill'
-su user0 -c 'zenity --info --text "Session loaded from '$path'" --title "Success!"' 
+rsync -a  --exclude-from=rsync-homedir-local.txt --progress $path/ /home/user0/ --delete | su user0 -c 'zenity --title "Session" --text "Loading...(Please wait for Ok Button)" --progress --auto-kill'
