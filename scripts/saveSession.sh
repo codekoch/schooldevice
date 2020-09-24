6f22b5efdac0cdc307bf4fc557ae57d50d5a122f
codekoch <45600398+codekoch@users.noreply.github.com>
2020-09-24 14:08:08 +0200

    Update saveSession.sh

--- scripts/saveSession.sh
+++ scripts/saveSession.sh
@@ -1,9 +1,9 @@
 
 #!/bin/bash  
-path=`su user0 -c zenity --file-selection --directory --title="Choose empty(!) folder for session backup" --filename=/media/user0/` 
+path=`su user0 -c 'zenity --file-selection --directory --title="Choose empty(!) folder for session backup" --filename=/media/user0/'` 
 cd /
-rsync -a /home/user0/ $path/ --delete | notify-send "Saving session" "please wait ..."
-notify-send "Session saved" "to $path"
+rsync -a /home/user0/ $path/ --delete | su user0 -c 'notify-send "Saving session" "please wait ..."'
+su user0 -c 'notify-send "Session saved" "to $path"'
  
 sleep 60
  
