c915632cab99b6fccb78ff4185348e8b6faf5d85
codekoch <45600398+codekoch@users.noreply.github.com>
2020-09-24 17:31:24 +0200

    Update saveSession.sh

--- scripts/saveSession.sh
+++ scripts/saveSession.sh
@@ -1,5 +1,5 @@
 #!/bin/bash  
-su user0 -c 'zenity --info --text "Please close all running aplications...\n" --title "ATTENTION!"' 
+su user0 -c 'zenity --info --width 300 --text "Please close all running aplications...\nAnd insert an external device with FAT32 filesystem" --title "ATTENTION!"' 
 path=`su user0 -c 'zenity --file-selection --directory --title="Choose a folder for session backup" --filename=/media/user0/'` 
 cd /
 wget https://raw.githubusercontent.com/codekoch/schooldevice/master/scripts/rsync-homedir-local.txt -O rsync-homedir-local.txt
