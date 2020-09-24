ff13ef8c5030c737710b78167bfa5001a5b18cab
codekoch <45600398+codekoch@users.noreply.github.com>
2020-09-24 15:52:33 +0200

    Delete notify.sh

--- scripts/notify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash                                            
- 
-sleep 10                                               
- 
-notify-send "ATTENTION:" "All local data will be lost during logout or restart!\nMake sure your data is backed up in the cloud or on an external device if necessary."   
- 
-                                     
-
