770a2c90392934e962245f90dc54db9b86bd01be
codekoch <45600398+codekoch@users.noreply.github.com>
2020-09-24 14:30:23 +0200

    Update loadSession.sh

--- scripts/loadSession.sh
+++ scripts/loadSession.sh
@@ -5,6 +5,4 @@ cd /
 rsync -a $path/ /home/user0/ --delete | su user0 -c 'notify-send "Loading session" "please wait ..."'
 su user0 -c 'notify-send "Session loaded" "from '$path'"'
  
-sleep 60                                                
- 
-pkill notify-send    
+
