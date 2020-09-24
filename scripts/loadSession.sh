7e6fd363221f7024a4e125659bc08f0dda63e972
codekoch <45600398+codekoch@users.noreply.github.com>
2020-09-24 16:23:14 +0200

    Update loadSession.sh

--- scripts/loadSession.sh
+++ scripts/loadSession.sh
@@ -2,7 +2,7 @@
 su user0 -c 'zenity --info --text "Please close all running aplications..." --title "ATTENTION!"' 
 path=`su user0 -c 'zenity --file-selection --directory --title="Choose folder with session backup" --filename=/media/user0/'` 
 cd /
-wget https://github.com/codekoch/schooldevice/blob/master/scripts/rsync-homedir-local.txt -O rsync-homedir-local.txt
+wget https://raw.githubusercontent.com/codekoch/schooldevice/master/scripts/rsync-homedir-local.txt -O rsync-homedir-local.txt
 chmod 777 rsync-homedir-local.txt
 rsync -a  --exclude-from=rsync-homedir-local.txt --progress $path/ /home/user0/ --delete | su user0 -c 'notify-send "Loading session" "please wait ..."'
 su user0 -c 'zenity --info --text "Session loaded from '$path'" --title "Success!"' 
