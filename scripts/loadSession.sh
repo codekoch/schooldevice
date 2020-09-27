a64a1983914c84fdd0c910e4e07a9028d6233354
codekoch <45600398+codekoch@users.noreply.github.com>
2020-09-27 12:06:58 +0200

    Update loadSession.sh

--- scripts/loadSession.sh
+++ scripts/loadSession.sh
@@ -4,4 +4,4 @@ path=`su user0 -c 'zenity --file-selection --directory --title="Choose folder wi
 cd /
 wget https://raw.githubusercontent.com/codekoch/schooldevice/master/scripts/rsync-homedir-local.txt -O rsync-homedir-local.txt
 chmod 777 rsync-homedir-local.txt
-rsync -a  --exclude-from=rsync-homedir-local.txt --progress $path/ /home/user0/ --delete | su user0 -c 'zenity --title "Session" --text "Loading from '$path'\nPlease wait for Ok Button..." --progress --pulsate --auto-kill'
+rsync -a  --exclude-from=rsync-homedir-local.txt --progress $path/ /home/user0/ --delete | su user0 -c 'zenity --title "Session" --text "Loading from '$path'.\nPlease wait for Ok Button..." --progress --pulsate --auto-kill'
