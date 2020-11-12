b8ea5a49f91ffbd192b88fd57ec7205ea4f1e27b
codekoch <45600398+codekoch@users.noreply.github.com>
2020-11-12 14:28:33 +0100

    Update software.sh

--- software.sh
+++ software.sh
@@ -22,8 +22,6 @@ sudo snap install arduino
 sudo snap install openboard
 
 ### via apt-get 
-sudo apt-get install arduino
-
 sudo apt-get install -y cura
 
 sudo apt-get install -y ballerburg
@@ -68,37 +66,24 @@ sudo apt-get update
 sudo apt-get install -y unetbootin
 
 #### Veyon
-sudo add-apt-repository ppa:veyon/stable -y
-sudo apt-get update
-sudo apt-get install -y veyon
+#sudo add-apt-repository ppa:veyon/stable -y
+#sudo apt-get update
+#sudo apt-get install -y veyon
 
 #### Linux Live Kit
-sudo apt-get install -y squashfs-tools
-sudo apt-get install -y genisoimage 
-sudo apt-get install -y zip 
-sudo apt-get install -y aufs-dkms 
-sudo apt-get install -y dkms
-git clone https://github.com/Tomas-M/linux-live
-sudo mkdir /a
-sudo  sed -i 's/VMLINUZ=\/vmlinuz/VMLINUZ=\/boot\/vmlinuz/g' linux-live/config
-sudo  sed -i 's|LIVEKITDATA=/tmp|LIVEKITDATA=/a|g' linux-live/config
-sudo cp -R linux-live /opt/
-sudo chmod -R 755 /opt/linux-live
-sudo echo '#!/bin/bash' > /usr/bin/buildLinuxLive.sh
-sudo echo 'cd /opt/linux-live' >> /usr/bin/buildLinuxLive.sh
-sudo echo './build' >> /usr/bin/buildLinuxLive.sh
-sudo chmod 755 /usr/bin/buildLinuxLive.sh
+#sudo apt-get install -y squashfs-tools
+#sudo apt-get install -y genisoimage 
+#sudo apt-get install -y zip 
+#sudo apt-get install -y aufs-dkms 
+#sudo apt-get install -y dkms
+#git clone https://github.com/Tomas-M/linux-live
+#sudo mkdir /a
+#sudo  sed -i 's/VMLINUZ=\/vmlinuz/VMLINUZ=\/boot\/vmlinuz/g' linux-live/config
+#sudo  sed -i 's|LIVEKITDATA=/tmp|LIVEKITDATA=/a|g' linux-live/config
+#sudo cp -R linux-live /opt/
+#sudo chmod -R 755 /opt/linux-live
+#sudo echo '#!/bin/bash' > /usr/bin/buildLinuxLive.sh
+#sudo echo 'cd /opt/linux-live' >> /usr/bin/buildLinuxLive.sh
+#sudo echo './build' >> /usr/bin/buildLinuxLive.sh
+#sudo chmod 755 /usr/bin/buildLinuxLive.sh
 
-#### Openboard
-#sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-#sudo flatpak install -y flathub ch.openboard.OpenBoard
-#sudo cp openboard.png /usr/share/pixmaps/
-#echo '[Desktop Entry]' > /usr/share/applications/openboard.desktop
-#echo 'Name=OpenBoard' >> /usr/share/applications/openboard.desktop
-#echo 'Comment=OpenBoard' >> /usr/share/applications/openboard.desktop
-#echo 'Type=Application' >> /usr/share/applications/openboard.desktop
-#echo 'Encoding=UTF-8' >> /usr/share/applications/openboard.desktop
-#echo 'Exec=/usr/bin/flatpak run ch.openboard.OpenBoard' >> /usr/share/applications/openboard.desktop
-#echo 'Icon=/usr/share/pixmaps/openboard.png' >> /usr/share/applications/openboard.desktop
-#echo 'Categories=GNOME;Application;Education;' >> /usr/share/applications/openboard.desktopecho 'Terminal=false' >> /usr/share/applications/openboard.desktop
-#echo 'StartupNotify=true' >> /usr/share/applications/openboard.desktop
