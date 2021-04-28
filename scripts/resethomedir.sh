c3cd1d64739d43f2f097992a2120c3f4ce69966c
codekoch <45600398+codekoch@users.noreply.github.com>
2021-04-28 16:11:14 +0200

    Update resethomedir.sh

--- scripts/resethomedir.sh
+++ scripts/resethomedir.sh
@@ -38,6 +38,7 @@ case "$1" in
          echo "Sealfhealing activated!"
       ;;
       deactivate)
+         sudo touch /etc/init.d/noresethomedirstart.sh
          sudo  sed -i 's/display-setup-script=.*$/display-setup-script=\/etc\/init.d\/noresethomedirstart.sh/g' /etc/lightdm/lightdm.conf.d/60-autologin.conf
          sudo sed -i 's/All local data will be lost during logout or restart!\\nMake sure your data is backed up in the cloud or on an external device if necessary./SELFHEAHLING DEACTIVATED!/g' /usr/bin/setbackground.sh
          echo "Sealfhealing deactivated!"
