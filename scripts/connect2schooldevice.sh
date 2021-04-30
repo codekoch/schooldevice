b943814eb2fc37b56869ed5ae4e93476fd85dc76
codekoch <45600398+codekoch@users.noreply.github.com>
2021-04-30 22:19:12 +0200

    Update connect2schooldevice.sh

--- scripts/connect2schooldevice.sh
+++ scripts/connect2schooldevice.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 ipaddress=$(zenity --width=400 --entry --title "IP ADDRESS of schooldevice to connecto to:" --text "Format: xxx.xxx.xxx.xxx")
-google-chrome "http://$ipaddress:8080/guacamole"
+exo-open --launch WebBrowser "http://$ipaddress:8080/guacamole"
